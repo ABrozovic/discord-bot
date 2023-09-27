a428c80ea982d5cbc5d6f52899e4ea2bf04ec5dc
ABrozovic <alejandro.brozovic@gmail.com>
2023-09-27 19:52:18 -0400

    fix: remove hyphens from svg params

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,9 +5,9 @@ const Icons = {
     <svg
       stroke="currentColor"
       fill="currentColor"
-      stroke-width="0"
+      strokeWidth="0"
       viewBox="0 0 16 16"
-      font-size="26"
+      fontSize="26"
       height="1em"
       width="1em"
       xmlns="http://www.w3.org/2000/svg"
