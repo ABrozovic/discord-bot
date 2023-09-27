638256236c4474f5c9ce716f0eaf19f97c176c6d
ABrozovic <alejandro.brozovic@gmail.com>
2023-09-27 19:53:47 -0400

    feat: add small hack to use react query cache

---
+++ src/hooks/use-my-query.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query"
+
+export const useMyQuery = <T>(queryKey: string[]) => {
+  const query = useQuery({ queryKey, queryFn: () => fakeFunc<T>() })
+
+  return query
+}
+
+export const fakeFunc = async <T>(): Promise<T> => {
+  return new Promise(() => {})
+}
